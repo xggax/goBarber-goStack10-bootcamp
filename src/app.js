9d98852230d3ed26707ea669b2dcd899f38b13e8
Gabriel Gama <gabrielgamagga@gmail.com>
2020-03-07 00:45:26 -0400

    estrutura de arquivos e configs inicial

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    // Para conseguir enviar requisições e receber respostas no formato de JSON da API.
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
