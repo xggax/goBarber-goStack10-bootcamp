1a68b091d5add8c7dcc772a239f55c9e3f2e38be
Gabriel Gama <gabrielgamagga@gmail.com>
2020-03-09 00:43:36 -0400

    backend: usuário, session, routes e middlewares

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'ca3c5a0bff20422a8b44c89f09d73f61',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,19 @@
-import Router from 'express';
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World!' }));
+/* Routes */
+
+// Users
+routes.post('/users', UserController.store);
+routes.put('/users', authMiddleware, UserController.update);
+
+// Sessions
+routes.post('/sessions', SessionController.store);
+
 export default routes;
